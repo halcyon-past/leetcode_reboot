085987a3d44cb73b0d72a435fb88e96c5a3fbbfb
Aritro Saha <70253408+halcyon-past@users.noreply.github.com>
2026-04-09 00:26:15 +0530

    Time: 648 ms (100%), Space: 32.6 MB (14.2%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        MOD = 1_000_000_007
+        mults = np.ones(len(nums), dtype=np.int64)
+        for l, r, k, v in queries:
+            mults[l:r+1:k] = mults[l:r+1:k] * v % MOD
+        arr = np.array(nums, dtype=np.int64) * mults % MOD
+        return int(np.bitwise_xor.reduce(arr))
